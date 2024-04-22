ae437e7e31d8c81aeb8123e64d4cc74971a83c08
Daniel Krois <danielkrois@hotmail.com>
2024-04-22 15:44:23 +0200

    added several different solutions, a readme and a gitignore

---
+++ Exercises/Coding17_CatsAndFood.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public static int NotHungryCats(string kitchen)
+        {
+            if (string.IsNullOrEmpty(kitchen) || !kitchen.Contains("F"))
+            {
+                throw new ArgumentException("Invalid kitchen string. It should contain at least one 'F'.");
+            }
+
+            string[] subStrings = kitchen.Split('F');
+            string kitchenLeftSide = subStrings[0].Replace(" ", "");
+            string kitchenRightSide = subStrings[1].Replace(" ", "");
+
+            if (kitchenLeftSide.Length % 2 != 0 || kitchenRightSide.Length % 2 != 0)
+            {
+                throw new ArgumentException("Invalid kitchen string. Each side should have an even number of characters.");
+            }
+
+            List<string> listCatsLeftSide = SplitIntoPairs(kitchenLeftSide);
+            List<string> listCatsRightSide = SplitIntoPairs(kitchenRightSide);
+
+            int notHungryCats = CountOccurrences(listCatsLeftSide, "O~") + CountOccurrences(listCatsRightSide, "~O");
+
+            return notHungryCats;
+        }
+        private static List<string> SplitIntoPairs(string input)
+        {
+            List<string> pairs = new List<string>();
+
+            for (int i = 0; i < input.Length; i += 2)
+            {
+                pairs.Add(input.Substring(i, 2));
+            }
+
+            return pairs;
+        }
+
+        private static int CountOccurrences(List<string> list, string target)
+        {
+            int count = 0;
+
+            foreach (var item in list)
+            {
+                if (item.Equals(target))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+Here you can find my solutions to the coding exercises of the Udemy course Complete C# Masterclass.
+This repo is not designed to be build and run, it serves only as documentation. I purposely decided not to refactor 
+any of the solutions in order to show my growth as a programmer. 
+
+If you have any questions for me please feel free to send me a message.
